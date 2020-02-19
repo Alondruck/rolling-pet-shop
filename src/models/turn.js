68f50428c01dff3e41acb50791c698a8f891cdee
hanshuettmann <hans.huttmann1@gmail.com>
2020-02-19 15:35:40 -0300

    turn model fixed

--- src/models/turn.js
+++ src/models/turn.js
@@ -3,48 +3,14 @@ import mongoose from 'mongoose';
 const appointmentSchema = new mongoose.Schema({
     userId: {
         type: String,
-        required: true,
-        default: null
+        required: true
     },
-    name: {
+    type: {
         type: String,
         required: true
     },
-    lastname:
-    {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    phone: {
+    date:{
         type: Number,
-        required: false
-    },
-    species: {
-        type: String,
-        required: true
-    },
-    race: {
-        type: String,
-        required: true
-    },
-    sex: {
-        type: String,
-        required: true
-    },
-    petName: {
-        type: String,
-        required: false
-    },
-    date: {
-        type: Date,
-        required: true
-    },
-    message: {
-        type: String,
         required: true
     }
 })
