51487ad796024ac506a1349bb5297f39e6306733
hanshuettmann <hans.huttmann1@gmail.com>
2020-02-19 15:34:19 -0300

    contact model fixed

--- src/models/contact.js
+++ src/models/contact.js
@@ -1,23 +1,69 @@
 import mongoose from 'mongoose';
 
 const contactSchema = new mongoose.Schema({
+    // email: {
+    //     type: String,
+    //     required: true
+    // },
+    // body: {
+    //     type: String,
+    //     required: true
+    // },
+    // date: {
+    //     type: Date,
+    //     required: true,
+    //     default: Date.now()
+    // },
+    // isChecked: {
+    //     type: Boolean,
+    //     required: true,
+    //     default: false
+    // }
+    userId: {
+        type: String,
+        required: true,
+        default: null
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    lastname:
+    {
+        type: String,
+        required: true
+    },
     email: {
         type: String,
         required: true
     },
-    body: {
+    phone: {
+        type: Number,
+        required: false
+    },
+    species: {
         type: String,
         required: true
     },
+    race: {
+        type: String,
+        required: true
+    },
+    sex: {
+        type: String,
+        required: true
+    },
+    petName: {
+        type: String,
+        required: false
+    },
     date: {
         type: Date,
-        required: true,
-        default: Date.now()
+        required: true
     },
-    isChecked: {
-        type: Boolean,
-        required: true,
-        default: false
+    message: {
+        type: String,
+        required: true
     }
 });
 
